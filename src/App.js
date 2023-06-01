212acc013c06fca37aa0f880eeef6a4cd0648bbd
Arash Ghafoori <ghafoa4@mcmaster.ca>
2023-06-01 00:46:38 -0700

    finalized

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import { Nav, NavBar } from './components/NavBar';
-import { Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from './components/navbar/NavBar';
+import { Banner } from './components/banner/Banner';
+import { Skills } from './components/skills/Skills';
+import { Projects } from './components/projects/Projects';
+import { Contact } from './components/contact/Contact';
+import { Footer } from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
         <NavBar />
+        <Banner />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
     </div>
   );
 }
